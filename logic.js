56dce991fb3cf95e601f4fa3c5d8a8550d6a7fa9
nabm-utn <nabm.utn@gmail.com>
2021-07-24 13:20:38 -0300

    small improvements

--- logic.js
+++ logic.js
@@ -9,92 +9,53 @@ function randomChoice(choices) {
     return choices[index];
 }
 
-function playRound(playerSelection, computerSelection) {
-    let answer = "";
-    playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection) {
-        answer = "It's a Tie! :s";
-    } else if (playerSelection === "rock") {
-        if (computerSelection === "paper") {
-            answer = "You Loose! Paper beats Rock :(";
-        } else {
-            answer = "You Win! Rock beats Scissors :)";
-        }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            answer = "You win! Paper beats Rock :)";
-        } else {
-            answer = "You Loose! Scissors beats Paper :(";
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            answer = "You Loose! Rock beats Scissors :(";
-        }
-        else {
-            answer = "You Win! Scissors beats Paper :)";
-        }
-    } else {
-        answer = "Your choice was invalid :/ Try again!"
-    }
-    console.log([playerSelection, computerSelection])
-    return answer;
-}
-
-function askPlayerSelection() {
-    const msg = "Type in your choice, either 'Rock', 'Paper' or 'Scissors' (case insensitive)...";
-    let answer = window.prompt(msg);
-    return answer.toLowerCase();
-}
-
-
-function bestOfFive() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    let winner = "";
-
-    for (let i = 1; i < 6; i++) {
-        let result = playRound(askPlayerSelection(), computerPlay());
-        if (result.includes("Win")) {
-            playerPoints += 1;
-        } else if (result.includes("Loose")) {
-            computerPoints += 1;
-        }
-        console.log(`Round ${i}:`)
-        console.log(result);
-        msg = `Player: ${playerPoints} | Computer: ${computerPoints} \n`
-        console.log(msg)
-    }
-
-    if (playerPoints > computerPoints) {
-        console.log("You have won the game! Congratulations!")
-    } else if (playerPoints < computerPoints) {
-        console.log("You have been beaten by the computer... Better luck next time!")
-    } else {
-        console.log("The game concludes in a draw. Better luck next time!")
-    }
-
-}
-
-let buttons = document.querySelectorAll('.choice-button');
-buttons.forEach(button => button.addEventListener("click", playRound2));
-
-function playRound2(e) {
+function playRound(e) {
     const log = document.querySelector(".log");
-    const playerScore = document.querySelector("#player-score");
-    const pcScore = document.querySelector("#pc-score");
     playerSelection = this.id;
     computerSelection = computerPlay();
     let message = roundMessage(playerSelection, computerSelection);
     let result = roundResult(message);
     log.appendChild(roundDiv(message, result));
     log.scrollTop = log.scrollHeight;
+
+
+    const playerScore = document.querySelector("#player-score");
+    const pcScore = document.querySelector("#pc-score");
     if (result === "WIN") {
         playerScore.textContent = parseInt(playerScore.textContent) + 1;
     } else if (result === "LOOSE") {
         pcScore.textContent = parseInt(pcScore.textContent) + 1;
     }
-    console.log("hey there!")
+
+    checkEndGame(playerScore, pcScore, log);
 }
+
+function checkEndGame(playerScore, pcScore, log) {
+    const playerScoreInt = parseInt(playerScore.textContent);
+    const pcScoreInt = parseInt(pcScore.textContent);
+    if (playerScoreInt >= 5 || pcScoreInt >= 5) {
+        let buttons = document.querySelectorAll('.choice-button');
+        buttons.forEach(button => button.removeEventListener("click", playRound));
+    } else return;
+
+    let winnerPs = null;
+    let finalMessage = null;
+    if (playerScoreInt > pcScoreInt) {
+        winnerPs = [document.querySelector("#player-name"), playerScore];
+        finalMessage = "Congratulations!\nYou have won the game!";
+    } else {
+        winnerPs = [document.querySelector("#pc-name"), pcScore];
+        finalMessage = "Sorry! You have been defeated!";
+    }
+    winnerPs[0].classList.toggle("winner-left");
+    winnerPs[1].classList.toggle("winner-right");
+    const finalDiv = roundDiv(finalMessage, "WIN");
+    finalDiv.classList.add("final-message");
+    log.appendChild(finalDiv);
+    log.scrollTop = log.scrollHeight;
+    
+}
+    
 
 function roundDiv(message, result) {
     const div = document.createElement("div");
@@ -145,3 +106,6 @@ function roundResult(message) {
     return result;
 
 }
+
+let buttons = document.querySelectorAll('.choice-button');
+buttons.forEach(button => button.addEventListener("click", playRound));
